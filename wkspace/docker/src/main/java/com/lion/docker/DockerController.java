2ce546e13a7f5e17d4f5d1b51495a7c35ec42296
yeonjuLee <yeonju509@gmail.com>
2024-12-14 09:53:54 +0900

    9일차: 도커 테스트 (base ver.)

--- wkspace/docker/src/main/java/com/lion/docker/DockerController.java
+++ wkspace/docker/src/main/java/com/lion/docker/DockerController.java
@@ -1,4 +1,12 @@
 package com.lion.docker;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DockerController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
